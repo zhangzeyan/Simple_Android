2f701b969459bac948f153268b9a8dc7e9adbdaf
zhangzeyan <zhangzeyaninzhuhai@gmail.com>
2017-07-10 18:52:19 +0800

    增加单元测试mockito,powermock

--- App/src/androidTest/java/com/simple/android/ExampleInstrumentedTest.java
+++ App/src/androidTest/java/com/simple/android/ExampleInstrumentedTest.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.simple.android.home.MainActivity;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -22,5 +24,23 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.simple.android", appContext.getPackageName());
+
+
+//        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
     }
 }
+
+//@RunWith(RobolectricTestRunner.class)
+//public class MyActivityTest {
+//
+//    @Test
+//    public void clickingButton_shouldChangeResultsViewText() throws Exception {
+//        MyActivity activity = Robolectric.setupActivity(MyActivity.class);
+//
+//        Button button = (Button) activity.findViewById(R.id.button);
+//        TextView results = (TextView) activity.findViewById(R.id.results);
+//
+//        button.performClick();
+//        assertThat(results.getText().toString()).isEqualTo("Robolectric Rocks!");
+//    }
+//}
--- App/src/test/java/com/simple/android/ExampleUnitTest.java
+++ App/src/test/java/com/simple/android/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package com.simple.android;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +10,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(1, 2);
+        Assert.assertEquals(3, sum);
+
+    }
+
+
+
+    public class Calculator {
+        public int add(int one, int another) {
+            //为了简单起见，暂不考虑溢出等情况。
+            return one + another ;
+        }
     }
 }
